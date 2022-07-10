1be268cf957b12d1b833c7b94e98320a576ff6a4
23-ryan <aryanmathe111@gmail.com>
2022-07-10 23:02:33 +0530

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,59 @@
 #include<iostream>
+#include<string>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
 using namespace std;
+
+class Form {
+	GLFWwindow* w;
+	GLuint width, height;
+	string name;
+	public:
+		Form(GLuint width, GLuint height, const string name) {
+			this->name = name;
+			this->width = width;
+			this->height = height;
+			w = glfwCreateWindow(width, height, name.c_str(), NULL, NULL);
+		}
+		
+		void show() {
+
+			glfwMakeContextCurrent(this->w);
+			glViewport(0, 0, width, height);
+			glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			glfwSwapBuffers(w);
+			while (!glfwWindowShouldClose(w)) {
+				glfwPollEvents();
+			}
+			glfwDestroyWindow(w);
+		}
+
+};
+	void draw(GLFWwindow* window) {
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+	{
+		glViewport(0, 0, width, height);
+		draw(window);
+	}
+	//void processInput(GLFWwindow* window)
+	//{
+	//	
+	//}
+	
+	void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+		
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
+
+		else std::cout << char(key) << std::endl;
+	}
 
 int main() {
 	glfwInit();
@@ -19,19 +70,24 @@ int main() {
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
-
 	gladLoadGL();
+	draw(window);
 
-	glViewport(0, 0, 800, 800);
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// render loop
 	while (!glfwWindowShouldClose(window)) {
+		// input
+		glfwSetKeyCallback(window, keyCallback);
+
+		//rendering commands here
+
+
+		//check and call events and swap the buffers
 		glfwPollEvents();
 	}
-
 
 	glfwTerminate();
 	return 0;
